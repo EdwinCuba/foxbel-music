9189a09eafb918293fc3d8486335b0e5e7e2014a
EdwinOlaffCubaDavila <edwincubad@gmail.com>
2021-05-02 15:35:20 -0500

    cors de produccion

--- src/assets/utils/searchSongByArtist.js
+++ src/assets/utils/searchSongByArtist.js
@@ -2,7 +2,7 @@ const searchSongByArtist = async id => {
   const corsDev = 'https://cors-anywhere.herokuapp.com/';
   const cors = '/cors-proxy/';
   const API = `https://api.deezer.com/artist/${id}/top?limit=50`;
-  const url = `${corsDev}${API}`;
+  const url = `${cors}${API}`;
 
   const response = await fetch(url);
   const data = await response.json();
--- src/assets/utils/searching.js
+++ src/assets/utils/searching.js
@@ -2,7 +2,7 @@ const searching = async input => {
   const corsDev = 'https://cors-anywhere.herokuapp.com/';
   const cors = '/cors-proxy/';
   const API = 'https://api.deezer.com/search?q=';
-  const url = `${corsDev}${API}${input};`
+  const url = `${cors}${API}${input};`
   const response = await fetch(url);
   const data = await response.json();
 
